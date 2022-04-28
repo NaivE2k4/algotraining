00154c4a471b9ab7514712c6d087371b5f6bb5aa
achebotarev <achebotarev@kraftway.ru>
2022-04-28 13:33:32 +0300

    Задача о рюкзаке на C#

--- DynProg/CSharpDynamicProg/Program.cs
+++ DynProg/CSharpDynamicProg/Program.cs
@@ -9,7 +9,10 @@ namespace CSharpDynamicProg
             //var res = Subsequences.PrefixFunc("abcabcasab");
             //var res = Subsequences.SubstringKMP("abcdabdeabfgabcdefgh", "abc");
             //var res = Subsequences.CoolerZFunc("abcabcasab");
-            var res = Subsequences.SubstringKMP("abcdabdeabfgabcdefgh", "abc", true);
+            //var res = Subsequences.SubstringKMP("abcdabdeabfgabcdefgh", "abc", true);
+            var masses = new int[] { 2, 1, 3 };
+            var values = new int[] { 4, 2, 5 };
+            var res = Backpack.DiscreteBackpack(5, masses, values);
             Console.ReadLine();
         }
     }
