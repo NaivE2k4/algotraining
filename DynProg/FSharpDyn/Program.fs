43ab253b5a57e5b15b3a03e8bfe8e978d021a8e5
Anton Chebotarev <ferdinand2k4@gmail.com>
2022-05-12 00:46:22 +0300

    Начало дин прогр на F# (Текущие функции хоть и работают, но ошибочны!

---
+++ DynProg/FSharpDyn/Program.fs
@@ -0,0 +1,14 @@
+﻿open subsequences
+
+[|3;1;2;3;4;5;4;7|]
+|> gis
+|> printfn "%A"
+
+
+[|3;1;2;3;4;5;4;7|]
+|> gis2
+|> printfn "%A"
+
+[|3;1;2;3;4;5;4;7|]
+|> gis3
+|> printfn "%A"
---
+++ DynProg/FSharpDyn/subsequences.fs
@@ -0,0 +1,47 @@
+﻿module subsequences
+//Функция для поиска длины наибольшей возрастающей подпоследовательности
+//Здесь сделано из своей головы, у Хирьянова поизящнее наверное, через максимум (см решение на питоне)
+//Решение ошибочно!
+let gis (arr:int[]) =
+    let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
+    for i = 1 to arr.Length-1 do
+        let mutable j = i - 1
+        while j>=0 && arr.[j] > arr.[i] do
+            j <- j-1
+        match j with
+            | -1 -> F.[i] <-1
+            | _ -> F.[i] <- F.[j] + 1
+    F
+
+//попробуем модифицировать в функциональном стиле
+let gis2 (arr:int[]) =
+    let first_lesser i (arr:int[]) =
+        let mutable j = i - 1
+        while j>=0 && arr.[j] > arr.[i] do
+            j <- j-1
+        j
+
+    let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
+    for i = 1 to arr.Length-1 do
+        match first_lesser i arr with
+            | -1 -> F.[i] <- 1
+            | j -> F.[i] <- F.[j] + 1
+    F
+
+//попробуем модифицировать в функциональном стиле
+let gis3 (arr:int[]) =
+    let rec mutator j i (arr:int[]) =
+        match j with
+            | -1 -> j
+            | x when arr.[x] < arr.[i] -> j
+            | _ -> mutator (j-1) i arr
+
+    let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
+    for i = 1 to arr.Length-1 do
+        match mutator (i-1) i arr with
+            | -1 -> F.[i] <- 1
+            | j -> F.[i] <- F.[j] + 1
+    F
+//Функция для поиска рассторяния Левенштейна
+//let levenstein str1 str2 = 
+    
