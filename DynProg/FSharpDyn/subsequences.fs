80c6ad7478f4a4398bdb3df382a081f1baa3580f
achebotarev <achebotarev@kraftway.ru>
2022-05-12 11:31:00 +0300

    Правильно работающая функция поиска НВП + несколько попыток написать ее в более функциональном стиле

--- DynProg/FSharpDyn/subsequences.fs
+++ DynProg/FSharpDyn/subsequences.fs
@@ -6,42 +6,75 @@ let gis (arr:int[]) =
     let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
     for i = 1 to arr.Length-1 do
         let mutable j = i - 1
-        while j>=0 && arr.[j] > arr.[i] do
+        let mutable max = 0
+        while j >=0  do
+            if arr.[j] < arr.[i] && F.[j] > max then
+                max <- F.[j]
             j <- j-1
-        match j with
-            | -1 -> F.[i] <-1
-            | _ -> F.[i] <- F.[j] + 1
+        match max with
+            | 0 -> F.[i] <-1
+            | _ -> F.[i] <- max + 1
     F
 
-//попробуем модифицировать в функциональном стиле
+//Попытка переписать в функциональном стиле 1
 let gis2 (arr:int[]) =
-    let first_lesser i (arr:int[]) =
+    let getmax i (arr:int[]) (F:int[])=
         let mutable j = i - 1
-        while j>=0 && arr.[j] > arr.[i] do
+        let mutable max = 0
+        while j >=0  do
+            if arr.[j] < arr.[i] && F.[j] > max then
+                max <- F.[j]
             j <- j-1
-        j
-
+        max
     let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
     for i = 1 to arr.Length-1 do
-        match first_lesser i arr with
-            | -1 -> F.[i] <- 1
-            | j -> F.[i] <- F.[j] + 1
+        match getmax i arr F with
+            | 0 -> F.[i] <-1
+            | max -> F.[i] <- max + 1
     F
 
-//попробуем модифицировать в функциональном стиле
+//Попытка переписать в функциональном стиле 2
 let gis3 (arr:int[]) =
-    let rec mutator j i (arr:int[]) =
+    let rec getmax j target curmax (arr:int[]) (F:int[]) =
         match j with
-            | -1 -> j
-            | x when arr.[x] < arr.[i] -> j
-            | _ -> mutator (j-1) i arr
+            | 0 -> curmax
+            | j -> 
+                if arr.[j] < target && F.[j] > curmax then
+                    getmax (j-1) target F.[j] arr F
+                else
+                    getmax (j-1) target curmax arr F
 
     let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
     for i = 1 to arr.Length-1 do
-        match mutator (i-1) i arr with
-            | -1 -> F.[i] <- 1
-            | j -> F.[i] <- F.[j] + 1
+        match getmax (i-1) arr.[i] 0 arr F with
+            | 0 -> F.[i] <-1
+            | max -> F.[i] <- max + 1
     F
+
+//Попытка переписать в функциональном стиле 3
+let gis4 (arr:int[]) =
+    let rec getmax j target curmax (arr:int[]) (F:int[]) =
+        match j with
+        | 0 -> curmax
+        | j -> 
+            match arr.[j], F.[j] with
+            | arrj, fj when arrj < target && fj > curmax -> getmax (j-1) target F.[j] arr F
+            | _ -> getmax (j-1) target curmax arr F
+
+    let rec iter (arr:int[]) (F:int[]) i =
+        match i with
+        | i when i = arr.Length -> ()
+        | _ ->  match getmax (i-1) arr.[i] 0 arr F with
+                | 0 -> F.[i] <-1
+                | max -> F.[i] <- max + 1
+                iter arr F (i+1)
+
+    let F = Array.init arr.Length (fun i -> if i = 0 then 1 else 0)
+    iter arr F 1
+    F
+//Пока мне не видно преимуществ в таком подходе, но я допускаю, что я пока не особо много чего понимаю
+
+
 //Функция для поиска рассторяния Левенштейна
 //let levenstein str1 str2 = 
     
